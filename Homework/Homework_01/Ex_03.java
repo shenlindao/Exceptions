15fb8f103a964b53b4736abdb59c032ba67affd1
Artem <shenlin@yandex.ru>
2023-09-08 19:40:03 +0300

    hw_01_ex_03

--- Homework/Homework_01/Ex_03.java
+++ Homework/Homework_01/Ex_03.java
@@ -1,3 +1,37 @@
+import java.util.Arrays;
 public class Ex_03 {
-    
+    public static class Answer {
+        public int[] divArrays(int[] a, int[] b) {
+            if (a.length != b.length) {
+                return new int[] { 0 };
+            } else {
+                int[] c = new int[a.length];
+                for (int i = 0; i < a.length; i++) {
+                    c[i] = a[i] / b[i];
+                }
+                return c;
+            }
+        }
+    }
+
+    // Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
+    public class Printer {
+        public static void main(String[] args) {
+            int[] a = {};
+            int[] b = {};
+
+            if (args.length == 0) {
+                // При отправке кода на Выполнение, вы можете варьировать эти параметры
+                a = new int[] { 12, 8, 16, 25 };
+                b = new int[] { 4, 2, 4 };
+            } else {
+                a = Arrays.stream(args[0].split(", ")).mapToInt(Integer::parseInt).toArray();
+                b = Arrays.stream(args[1].split(", ")).mapToInt(Integer::parseInt).toArray();
+            }
+
+            Answer ans = new Answer();
+            String itresume_res = Arrays.toString(ans.divArrays(a, b));
+            System.out.println(itresume_res);
+        }
+    }
 }
