23cbf4a7b713e73289fe337d4c860de34190a199
Artem <shenlin@yandex.ru>
2023-09-11 13:00:01 +0300

    hw_02_ex_02

--- Homework/Homework_02/Ex_02.java
+++ Homework/Homework_02/Ex_02.java
@@ -1,5 +1,23 @@
 package Homework.Homework_02;
 
 public class Ex_02 {
-    
+    public static void main(String[] args) {
+        catchedRes1();
+    }
+
+    public static void catchedRes1() {
+        // Если массив меньше размером, будет поймана ошибка IndexOutOfBoundsException
+        // int[] intArray = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        int[] intArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        try {
+            // При делении на ноль будет поймана ошибка ArithmeticException
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Catching exception: " + e);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+    }
 }
