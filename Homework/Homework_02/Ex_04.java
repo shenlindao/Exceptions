f69265229d0083c5ec2cbef82d642f7fa182e8e0
Artem <shenlin@yandex.ru>
2023-09-11 16:15:37 +0300

    hw_02_ex_04

--- Homework/Homework_02/Ex_04.java
+++ Homework/Homework_02/Ex_04.java
@@ -1,5 +1,27 @@
 package Homework.Homework_02;
 
+import java.util.Scanner;
+
 public class Ex_04 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        getString(scanner);
+    }
+
+    public static void getString(Scanner scanner) {
+        System.out.print("Введите строку: \n");
+        String string = scanner.nextLine();
+        try {
+            if (string.equals("")) {
+                throw new RuntimeException("\nСтрока не должна быть пустой!\n");
+            } else {
+                System.out.println("Вы ввели: " + string);
+            }
+        } catch (Exception e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
+
+    }
 }
