7e5a6a55ffa38874b65a68f9f191e2393a9ee0cc
Artem <shenlin@yandex.ru>
2023-09-11 13:30:12 +0300

    hw_02_ex_03

--- Homework/Homework_02/Ex_03.java
+++ Homework/Homework_02/Ex_03.java
@@ -1,5 +1,27 @@
 package Homework.Homework_02;
 
+import java.io.FileNotFoundException;
+
 public class Ex_03 {
-    
+    public static void main(String[] args) throws Exception {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2 };
+            abc[1] = 9;
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) {  // должен обрабатываться после всех остальных исключений
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+
+    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+        System.out.println(a + b);
+    }
+
 }
